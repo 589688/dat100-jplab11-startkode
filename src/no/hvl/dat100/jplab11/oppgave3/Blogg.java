370d3452a17171e66b6c7509d811c0974c7c8fe7
Jørgen <Jrgen@LAPTOP-EOPI4NUG.lan>
2020-11-12 20:29:36 +0100

    Java programmering 3

--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -5,63 +5,120 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	Innlegg innleggtabell[];
+	int nesteledig = 0;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[20];
+		System.out.println("test");
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell = new Innlegg[lengde];
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteledig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggtabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
+		int funnet = -1;
+		
+		for (int i=0;i<nesteledig && funnet == -1;i++) {
+			if (innleggtabell[i].erLik(innlegg)) {
+				funnet = i;
+			}
+		}
+		return funnet;
+		}
+	
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+		boolean funnet = false;
+		for (int i=0;i<nesteledig && !funnet;i++) {
+			if (innleggtabell[i].erLik(innlegg)) 
+				funnet = true;
+		}	
+		return funnet;
+		}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		if (nesteledig < innleggtabell.length)
+			return true;
+		
+		else 
+			return false;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		boolean lagtTil = false;
+		
+		if (!finnes(innlegg)) {
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			lagtTil = true;
+		}
+		return lagtTil;
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		String streng = nesteledig + "\n" ;
+		
+		for (int i=0; i<nesteledig; i++) {
+		streng += innleggtabell[i].toString();
+		}
+		return streng;
 	}
 
 	// valgfrie oppgaver nedenfor
+
 	
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+
+		Innlegg[] nyTab = new Innlegg[innleggtabell.length * 2];
+		
+		for (int i=0; i<innleggtabell.length; i++) {
+			nyTab[i] = innleggtabell[i];
+		}
+		
+		innleggtabell = nyTab;
 	}
 	
 	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		
+		boolean sattInn = false;
+		if (nesteledig < innleggtabell.length && !finnes(innlegg)) {
+			sattInn = leggTil(innlegg);
+		}
+		else if (nesteledig >= innleggtabell.length && !finnes(innlegg)) {
+			utvid();
+			sattInn = leggTil(innlegg);
+		}
+		
+		return sattInn;
 		
 	}
 	
 	public boolean slett(Innlegg innlegg) {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		int pos = finnInnlegg(innlegg);
+		
+		if (pos >= 0) {
+			nesteledig--;
+			innleggtabell[pos] = innleggtabell[nesteledig];
+			innleggtabell[nesteledig] = null;
+			return true;
+			
+		}
+		
+		else
+			return false;
 	}
 	
 	public int[] search(String keyword) {
--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -8,8 +8,36 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
+	
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		boolean written = true;
+		
+		try {
+			//åpne ny fil
+			//skrive til filen så lenge det er flere objekter i tabellen
+			//bruke blogg sin toString, 
+			//skrive til blogg.dat¨
+			String fn = mappe + filnavn;
+			
+			PrintWriter skriver = new PrintWriter(fn);
+			
+			skriver.println(samling.toString());
+			
+			skriver.print("Alle innlegg er skrevet");
+			
+			skriver.close();
+		}
+			 
+		catch (FileNotFoundException e) {
+			System.out.println("En feil oppstod ved skriving til fil");
+			written = false;
+		}
+			
+		return written;		
 	}
-}
+	}
+  	
+		
+	
+	
